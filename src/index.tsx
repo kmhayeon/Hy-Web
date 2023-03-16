34a035d9f76abc01723acd5a0ff7aec2eee2121d
hayeon <rian@oscnco.com>
2023-03-16 14:55:22 +0900

    [Add] 폰트 적용 코드 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../src/styles/global.css';
+import "./styles/font.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
